6cd8a63d18a9f1c1670451b5944c1234536038c0
HasanMammadov <hasanmamedoff@gmail.com>
2020-02-29 14:19:41 -0500

    hasan

---
+++ src/myFolder/Main.java
@@ -0,0 +1,11 @@
+package myFolder;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello B15");
+        System.out.println("I swear God it took 1 hour. I was feel so dumb");
+    }// whatever i wrote here it will not be code untill i remove slash
+    
+}
